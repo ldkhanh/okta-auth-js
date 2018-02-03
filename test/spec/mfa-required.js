df2162d13db6cd06498711b6de899fd49f074435
Gowthami <gowthami.dommety@okta.com>
2018-02-02 18:41:19 -0800

    :seedling: Adds support for autoPush url param in poll function
    Resolves: OKTA-155565

--- test/spec/mfa-required.js
+++ test/spec/mfa-required.js
@@ -107,6 +107,135 @@ define(function(require) {
           });
         }
       });
+      util.itMakesCorrectRequestResponse({
+        title: 'passes autoPush as a query param if true',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify?autoPush=true',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: true
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'passes autoPush as a query param if false',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify?autoPush=false',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: false
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'doesn\'t pass autoPush as a query param if undefined',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: undefined
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'passes autoPush and rememberDevice as a query param if true',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify?autoPush=true&rememberDevice=true',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: true,
+            rememberDevice: true
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'passes autoPush as a query param if autoPush is true and rememberDevice is false',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify?autoPush=true',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: true,
+            rememberDevice: false
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'passes rememberDevice as a query param if autoPush is undefined and rememberDevice is true',
+        setup: {
+          status: 'mfa-required',
+          request: {
+            uri: '/api/v1/authn/factors/uftigiEmYTPOmvqTS0g3/verify?rememberDevice=true',
+            data: {
+              stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+              passCode: '123456'
+            }
+          },
+          response: 'success'
+        },
+        execute: function (test) {
+          var factor = _.find(test.trans.factors, {id: 'uftigiEmYTPOmvqTS0g3'});
+          return factor.verify({
+            passCode: '123456',
+            autoPush: undefined,
+            rememberDevice: true
+          });
+        }
+      });
       util.itErrorsCorrectly({
         title: 'returns correct error when invalid answer provided (403)',
         setup: {
@@ -168,6 +297,54 @@ define(function(require) {
             return factor.verify({
               passCode: '123456',
               rememberDevice: true
+            });
+          })
+          .fin(function() {
+            expect(invalidError).not.toBeUndefined();
+          });
+        }
+      });
+      util.itMakesCorrectRequestResponse({
+        title: 'allows correct answer after invalid answer with autoPush',
+        setup: {
+          status: 'mfa-required',
+          calls: [
+            {
+              request: {
+                uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?autoPush=true',
+                data: {
+                  stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+                  passCode: 'invalidanswer'
+                }
+              },
+              response: 'mfa-required-error'
+            },
+            {
+              request: {
+                uri: '/api/v1/authn/factors/smsigwDlH85L9FyQK0g3/verify?autoPush=true',
+                data: {
+                  stateToken: '004KscPNUS2LswGp26qiu4Hetqt_zcgz-PcQhPseVP',
+                  passCode: '123456'
+                }
+              },
+              response: 'success'
+            }
+          ]
+        },
+        execute: function (test) {
+          var invalidError;
+          var factor = _.find(test.trans.factors, {id: 'smsigwDlH85L9FyQK0g3'});
+          return factor.verify({
+            passCode: 'invalidanswer',
+            autoPush: true
+          })
+          .fail(function(err) {
+            invalidError = err;
+          })
+          .then(function() {
+            return factor.verify({
+              passCode: '123456',
+              autoPush: true
             });
           })
           .fin(function() {
